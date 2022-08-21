5f47af7b214c58d38dd009f9919ddb38842cc04c
muratbodur <muratbodur03@hotmail.com>
2022-08-21 13:55:51 +0300

    Asp.Net Core 5.0 Blog Calismasi 1.Asama
    Asp.Net Core 5.0 Blog Calismasi 1.Asama

---
+++ CoreDemo/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,29 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+   public class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DEVELOPER;Initial Catalog=CoreBlogDb;integrated security=true; User ID=sa;Password=2697");
+
+
+        }
+
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+    }
+}
